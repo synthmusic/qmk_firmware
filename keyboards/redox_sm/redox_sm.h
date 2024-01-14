31a51a595e40683ea3da58e5be6bb36a4a692449
synthmusic <synthmusic@gmail.com>
2024-01-13 19:43:44 -0800

    update to 44 key layout

--- keyboards/redox_sm/redox_sm.h
+++ keyboards/redox_sm/redox_sm.h
@@ -3,20 +3,20 @@
 #include "quantum.h"
 
 #define LAYOUT( \
-  k15, k16, k17, k18, k19, k06,                k07, k22, k23, k24, k25, k26, \
-  k29, k30, k31, k32, k33, k20,                k21, k36, k37, k38, k39, k40, \
-  k43, k44, k45, k46, k47, k34, k48,      k49, k35, k50, k51, k52, k53, k54, \
-       k58, k59,    k60,   k61, k62,      k63, k64,   k65,    k66, k67       \
+  l00, l01, l02, l03, l04,                          r41, r42, r43, r44, r45, \
+  l10, l11, l12, l13, l14, l05,                r40, r51, r52, r53, r54, r55, \
+  l20, l21, l22, l23, l24, l15,                r50, r61, r62, r63, r64, r65, \
+       l31, l32,    l33,   l34, l35,      r70, r71,   r72,    r73, r74       \
 ) \
 { \
-    { KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,k06  }, \
-    { k15,  k16,  k17,  k18,  k19,  k20  }, \
-    { k29,  k30,  k31,  k32,  k33,  k34  }, \
-    { k43,  k44,  k45,  k46,  k47,  k48  }, \
-    { KC_NO,k58,  k59,  k60,  k61,  k62  }, \
-    { k07,  KC_NO,KC_NO,KC_NO,KC_NO,KC_NO}, \
-    { k21,  k22,  k23,  k24,  k25,  k26  }, \
-    { k35,  k36,  k37,  k38,  k39,  k40  }, \
-    { k49,  k50,  k51,  k52,  k53,  k54  }, \
-    { k63,  k64,  k65,  k66,  k67,  KC_NO}  \
+    { KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO}, \
+    { l00,  l01,  l02,  l03,  l04,  l05  }, \
+    { l10,  l11,  l12,  l13,  l14,  l15  }, \
+    { l20,  l21,  l22,  l23,  l24,  KC_NO}, \
+    { KC_NO,l31,  l32,  l33,  l34,  l35  }, \
+    { KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO}, \
+    { r40,  r41,  r42,  r43,  r44,  r45  }, \
+    { r50,  r51,  r52,  r53,  r54,  r55  }, \
+    { KC_NO,r61,  r62,  r63,  r64,  r65  }, \
+    { r70,  r71,  r72,  r73,  r74,  KC_NO}  \
 }
