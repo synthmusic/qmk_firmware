bf629e499006aa211773465883988922c5b76368
synthmusic <synthmusic@gmail.com>
2025-03-04 08:36:49 -0800

    fix physical problem w/ code

--- keyboards/redox_sm/right/config.h
+++ keyboards/redox_sm/right/config.h
@@ -1,3 +1,4 @@
 // wiring of each half ----  RIGHT
-#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { C6, E6, B4, B5 }
+// #define MATRIX_ROW_PINS { D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6 }
