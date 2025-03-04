4e7f7d7d3221d7021e4378d96d714538bdb7964f
synthmusic <synthmusic@gmail.com>
2025-03-04 10:50:03 -0800

    add console and dev

--- keyboards/redox_sm/qmk.sh
+++ keyboards/redox_sm/qmk.sh
@@ -14,8 +14,17 @@ if [ $argument == "compile" ]; then
     qmk compile -kb redox_sm -km default
 elif [ $argument == "help" ]; then
     echo "compile: Compiles only"
+    echo "console: runs the console"
+    echo "dev: jump to the dev directory"
+elif [ $argument == "console" ]; then
+    echo "Running console monitoring"
+    qmk console
+elif [ $argument == "dev" ]; then
+    echo "NOT WORKING FOR SOME REASON -- look at the command in script.  Moving to the dev directory"
+    cd src/repos/qmk/qmk_firmware/keyboards/redox_sm/
 elif [ $argument == "command2" ]; then
     echo "Running command 2..."
 else
     echo "Invalid argument. Please provide a valid argument."
 fi
+
